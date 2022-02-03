470821a7bfdd01ed03d051df11203289993b645c
timoanttila <14306628+timoanttila@users.noreply.github.com>
2022-02-03 20:52:00 +0200

    Header and Summary

--- src/App.js
+++ src/App.js
@@ -13,30 +13,37 @@ function App() {
 	}, []);
 
 	return (
-		<ul id="companies">
-			{data.map(item => (
-				<li key={item.id}>
-					<div className="title">
-						<a href={item.link}>{item.name}</a>
-					</div>
-					<div className="description">
-						<span>{item.description}</span>
-					</div>
-					{(item.areaName || item.company) &&
-						<div className="area">
-							<small>
-								{item.areaName &&
-									<span>{item.areaName}</span>
-								}
-								{item.company &&
-									<span>{item.company}</span>
-								}
-							</small>
+		<main>
+			<div className="container">
+				<h1>Suomalaiset verkkokaupat</h1>
+				<p>Alati kasvava lista aktiivisista luotettavista suomalaisista verkkokaupoista. Tue kotimaisia yrityksiä ja lähituotantoa, osta suomalaiselta.</p>
+			</div>
+
+			<ul id="companies">
+				{data.map(item => (
+					<li key={item.id}>
+						<div className="title">
+							<a href={item.link}>{item.name}</a>
 						</div>
-					}
-				</li>
-			))}
-		</ul>
+						<div className="description">
+							<span>{item.description}</span>
+						</div>
+						{(item.areaName || item.company) &&
+							<div className="area">
+								<small>
+									{item.areaName &&
+										<span>{item.areaName}</span>
+									}
+									{item.company &&
+										<span>{item.company}</span>
+									}
+								</small>
+							</div>
+						}
+					</li>
+				))}
+			</ul>
+		</main>
 	);
 }
 
